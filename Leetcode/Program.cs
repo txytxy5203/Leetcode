4e32ebc085837a4f32a92671d3960db3aae6565d
学有 谈 <168973291@qq.com>
2025-08-14 22:22:21 +0800

    增加了一些二叉树的内容

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,45 +1,154 @@
-﻿using System.Collections.Generic;
-
-namespace Leetcode
+﻿namespace Leetcode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //PriorityQueue<int,int> heap = new PriorityQueue<int, int>();
-            //HashSet<string> set = new HashSet<string>();
-            TreeNode a = new TreeNode(1);
-            TreeNode b = new TreeNode(2);
-            TreeNode c = new TreeNode(3);
-            TreeNode d = new TreeNode(4);
-            TreeNode e = new TreeNode(5);
-            TreeNode f = new TreeNode(6);
-            TreeNode g = new TreeNode(7);
-            a.left = b; a.right = c;
-            b.left = d; b.right = e;
-            d.left = null;d.right = null;
-            e.left = null;e.right = null;
-            c.left = f; c.right = g;
-            f.left = null;f.right = null;
-            g.left = null;g.right = null;
+            //TreeNode a = new TreeNode(1);
+            //TreeNode b = new TreeNode(2);
+            //TreeNode c = new TreeNode(3);
+            //TreeNode d = new TreeNode(4);
+            //TreeNode e = new TreeNode(5);
+            //TreeNode f = new TreeNode(6);
+            //TreeNode g = new TreeNode(7);
+            //a.left = b; a.right = c;
+            //b.left = d; b.right = e;
+            //d.left = null; d.right = null;
+            //e.left = null; e.right = null;
+            //c.left = f; c.right = g;
+            //f.left = null; f.right = null;
+            //g.left = null; g.right = null;
+            TreeNode a5 = new TreeNode(5);
+            TreeNode a3 = new TreeNode(3);
+            TreeNode a7 = new TreeNode(7);
+            TreeNode a2 = new TreeNode(2);
+            TreeNode a4 = new TreeNode(4);
+            TreeNode a6 = new TreeNode(6);
+            TreeNode a8 = new TreeNode(8);
+            TreeNode a1 = new TreeNode(1);
+            a5.left = a3; a5.right = a7;
+            a3.left = a2; a3.right = a4;
+            a7.left = a6; a7.right = a8;
+            a2.left = a1; a2.right = null;
+            a4.left = null; a4.right = null;
+            a6.left = null; a6.right = null;
+            a8.left = null; a8.right = null;
+            a1.left = null; a1.right = null;
 
-            preOrderUnRecur(a);
+
+            Console.WriteLine(IsValidBST(a5));
+            
+
         }
         #region 二叉树
+        static bool IsValidBST(TreeNode root)
+        {
+            // https://leetcode.cn/problems/validate-binary-search-tree/description/
+            // 傻白甜写法
+            IList<int> list = new List<int>();
+            //中序遍历的时候已经生成了list 检查list即可
+            inOrderRecur(root, list);
+            for (int i = 0; i < list.Count - 1; i++) 
+            {
+                if (!(list[i] < list[i + 1]))
+                    return false;
+            }
+            return true;
+        }
+        static bool IsValidBST2(TreeNode root)
+        {
+            // LeetCode题解中的解法   好好看看 仔细体会一下递归的思想
+            return IsValidBST2(root, long.MinValue, long.MaxValue);
+        }
+        static bool IsValidBST2(TreeNode node, long lower, long upper)
+        {
+            if(node == null) 
+                return true;
+
+            if(node.val < lower || node.val > upper)
+                return false;
+            return IsValidBST2(node.left, lower, node.val) && IsValidBST2(node.right, node.val, upper);
+        }
+        static int WidthOfBinaryTree(TreeNode root)
+        {
+            // https://leetcode.cn/problems/maximum-width-of-binary-tree/description/
+            // 队列中不仅要包含它的TreeNode 还要有它的编号
+            // 最后通过编号来计算宽度
+            if (root == null) return 0;
+            Queue<(TreeNode, int)> queue = new Queue<(TreeNode, int)>();
+            
+            int currId = 0;
+            int max = 1;
+
+            queue.Enqueue((root, currId));
+            while (queue.Count > 0)
+            {
+                // 一次性把一层的节点全部弹出  这样就做到了层序遍历
+                int temp = queue.Count;
+                // 记录每一层的第一个和最后一个节点Id
+                int first = 0;
+                int last = 0;
+                for (int i = 0; i < temp; i++)
+                {
+                    TreeNode node;
+                    (node, currId) = queue.Dequeue();
+                    if(i == 0)
+                        first = currId;
+                    if(i == temp - 1)
+                        last = currId;
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue((node.left, currId * 2 + 1));
+                    }                 
+                    if (node.right != null)
+                    {
+                        queue.Enqueue((node.right, currId * 2 + 2));
+                    }
+                }
+                max = Math.Max(max, last - first + 1);              
+            }
+            return max;
+        }
+        static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            // https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
+            // 二叉树的层序遍历 也叫 宽度遍历   BFS   leetcode上面的题解讲得很好
+            // https://leetcode.cn/problems/binary-tree-level-order-traversal/solutions/244853/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
+            if (root == null) return new List<IList<int>>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();           
+            queue.Enqueue(root);
+
+            IList<IList<int>> res = new List<IList<int>>();
+
+            while (queue.Count > 0)
+            {
+                // 一次性把一层的节点 全部弹出  这样就做到了层序遍历
+                int temp = queue.Count;
+                IList<int> list = new List<int>();
+                for (int i = 0; i < temp; i++)
+                { 
+                    TreeNode node = queue.Dequeue();
+                    list.Add(node.val);
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                res.Add(list);
+            }
+            return res;
+        }
         static IList<int> InorderTraversal(TreeNode root)
         {
             IList<int> result = new List<int>();
             preOrderRecur(root, result);
             return result;
-        }
-        static void inOrderRecur(TreeNode head, IList<int> list)
-        {
-            //中序遍历  在第二次进入节点的时候 执行逻辑
-            if (head == null)
-                return;
-            inOrderRecur(head.left, list);
-            list.Add(head.val);
-            inOrderRecur(head.right, list);
         }
         static void preOrderRecur(TreeNode head, IList<int> list)
         {
@@ -50,9 +159,19 @@ namespace Leetcode
             preOrderRecur(head.left, list);
             preOrderRecur(head.right, list);
         }
+        static void inOrderRecur(TreeNode head, IList<int> list)
+        {
+            //中序遍历  在第二次进入节点的时候 执行逻辑
+            if (head == null)
+                return;
+            inOrderRecur(head.left, list);
+            list.Add(head.val);
+            inOrderRecur(head.right, list);
+        }
+
         static void preOrderUnRecur(TreeNode head)
         {
-            //不使用递归的方式
+            //前序遍历 不使用递归的方式
             if (head == null) return;
             Stack<TreeNode> stack = new Stack<TreeNode>();
             stack.Push(head);
@@ -66,13 +185,57 @@ namespace Leetcode
                     stack.Push(node.left);
             }
         }
+        static void inOrderUnRecur(TreeNode head)
+        {
+            // 中序遍历 不使用递归
+            // 这个代码要好好理解一下
+            if (head == null) return;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            while (stack.Count > 0 || head != null)
+            {
+                if (head != null)
+                {
+                    stack.Push(head);
+                    head = head.left;
+                }
+                else
+                {
+                    head = stack.Pop();
+                    Console.WriteLine(head.val);
+                    head = head.right;
+                }
+            }
+        }
+
+        static void posOrderUnRecur(TreeNode head)
+        {
+            //后续遍历  不使用递归的方式
+            if (head == null) return;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            Stack<TreeNode> printStack = new Stack<TreeNode>();
+            stack.Push(head);
+            while (stack.Count > 0)
+            {
+                TreeNode node = stack.Pop();
+                printStack.Push(node);
+                if (node.left != null)
+                    stack.Push(node.left);
+                if (node.right != null)
+                    stack.Push(node.right);
+            }
+            while (printStack.Count > 0)
+            {
+                Console.WriteLine(printStack.Pop().val);
+            }
+        }
         #endregion
 
         static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
         {
             // 对齐的思想 因为没有环的存在
             // 下次试一下 HashSet
-            if (headA == null || headB == null) 
+            if (headA == null || headB == null)
                 return null;
 
             ListNode currA = headA;
@@ -81,12 +244,12 @@ namespace Leetcode
             int lengthB = 1;
             while (true)
             {
-                if(currA.next != null)
+                if (currA.next != null)
                 {
                     currA = currA.next;
                     lengthA++;
                 }
-                if(currB.next != null)
+                if (currB.next != null)
                 {
                     currB = currB.next;
                     lengthB++;
@@ -94,8 +257,8 @@ namespace Leetcode
                 if (currA.next == null && currB.next == null)
                     break;
             }
-                      
-            if (currA != currB) 
+
+            if (currA != currB)
                 return null;
 
 
@@ -104,7 +267,7 @@ namespace Leetcode
             if (lengthA >= lengthB)
             {
                 int offset = lengthA - lengthB;
-                while(offset > 0)
+                while (offset > 0)
                 {
                     currA = currA.next;
                     offset--;
@@ -116,7 +279,7 @@ namespace Leetcode
                 }
                 return currA;
             }
-            else 
+            else
             {
                 int offset = lengthB - lengthA;
                 while (offset > 0)
